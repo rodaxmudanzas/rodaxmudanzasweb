c943ac4f8fbfd3dc3858813a974a71bafa924423
rodaxmudanzas <rodax.mudanzas.01@gmail.com>
2026-06-07 20:53:43 +0200

    Update script.js
    c

--- script.js
+++ script.js
@@ -347,7 +347,7 @@ if(toggleMudanzaTotal){
     
     // ─── 1. CONFIGURACIÓN SUPABASE ────────────────────────────────────────────
     const SUPABASE_URL = 'https://defvfvyfrydopaybnisg.supabase.co';
-    const SUPABASE_KEY = 'sb_publishable_DfBOPKC1US-oy8-_O3hu6w__RBAJGWY';
+    const SUPABASE_KEY = 'sb_publishable_dKIRJ1_K4u1ypfCNTuylIw_AyAKeZTS';
     const supabase     = window.supabase ? window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY) : null;
 
     // ─── 2. REFERENCIAS UI ───────────────────────────────────────────────────
