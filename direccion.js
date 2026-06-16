715de314f0dc2f9e41a5ae8c7b214c47bf329934
rodaxmudanzas <rodax.mudanzas.01@gmail.com>
2026-06-16 12:43:16 +0200

    Update direccion.js
    Preparar direccion.js

--- direccion.js
+++ direccion.js
@@ -1,1 +1,26 @@
+/*
+=========================================
+RODAX DIRECCIONES
+OpenRouteService
+=========================================
+*/
 
+const ORS_API_KEY =
+'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjViY2ZiNmRiN2EzZDQxMzJhZDlmMmY4NGIyN2RlNzVmIiwiaCI6Im11cm11cjY0In0=';
+
+let origenSeleccionado = null;
+let destinoSeleccionado = null;
+
+const oriInput =
+document.getElementById('origen');
+
+const desInput =
+document.getElementById('destino');
+
+const oriDropdown =
+document.getElementById('origen-dropdown');
+
+const desDropdown =
+document.getElementById('destino-dropdown');
+
+console.log('direccion.js cargado');
