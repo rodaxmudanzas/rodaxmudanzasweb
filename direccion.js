d943b2a05cca6f3386a57c5403bb50947e56e4d7
rodaxmudanzas <rodax.mudanzas.01@gmail.com>
2026-06-16 15:43:15 +0200

    Update direccion.js
    c

--- direccion.js
+++ direccion.js
@@ -8,9 +8,6 @@ OpenRouteService
 const ORS_API_KEY =
 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjViY2ZiNmRiN2EzZDQxMzJhZDlmMmY4NGIyN2RlNzVmIiwiaCI6Im11cm11cjY0In0=';
 
-let origenSeleccionado = null;
-let destinoSeleccionado = null;
-
 const oriInput =
 document.getElementById('origen');
 
