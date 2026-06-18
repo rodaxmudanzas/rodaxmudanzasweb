08f2885294f9615cdc4e4818f7d9c19a91105cb1
rodaxmudanzas <rodax.mudanzas.01@gmail.com>
2026-06-18 13:13:47 +0200

    Update create-checkout-session.js
    c

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -4,9 +4,62 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 module.exports = async (req, res) => {
 
-    res.status(200).json({
-        ok: true,
-        mensaje: "Stripe API funcionando"
-    });
+    try {
+
+        const session = await stripe.checkout.sessions.create({
+
+            payment_method_types: ["card"],
+
+            mode: "payment",
+
+            line_items: [
+
+                {
+
+                    price_data: {
+
+                        currency: "eur",
+
+                        product_data: {
+
+                            name: "Reserva RODAX"
+
+                        },
+
+                        unit_amount: 5000
+
+                    },
+
+                    quantity: 1
+
+                }
+
+            ],
+
+            success_url: "https://rodaxmudanzasweb.vercel.app",
+
+            cancel_url: "https://rodaxmudanzasweb.vercel.app"
+
+        });
+
+        res.status(200).json({
+
+            id: session.id
+
+        });
+
+    }
+
+    catch (err) {
+
+        console.error(err);
+
+        res.status(500).json({
+
+            error: err.message
+
+        });
+
+    }
 
 };
