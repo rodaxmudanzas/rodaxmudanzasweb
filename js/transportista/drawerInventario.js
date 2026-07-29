494daad4e0c3eb40d4dc8fa5b4da8b46969fd85a
Juan Flores Seabra <rodax.mudanzas.01@gmail.com>
2026-07-29 11:08:31 +0200

    c

---
+++ js/transportista/drawerInventario.js
@@ -0,0 +1,17 @@
+(function () {
+
+    "use strict";
+
+    console.log("drawerInventario.js cargado");
+
+    window.renderInventarioDrawer = function (mudanza) {
+
+        console.log("renderInventarioDrawer ejecutado");
+
+        console.log(mudanza);
+
+        console.log("Inventario:", mudanza.inventario);
+
+    };
+
+})();
